9f43e300248b48fe2d6b416f005ddb5fc5a99f1f
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2016-11-14 13:06:15 +1100

    xive: Provide a way to override some IPI sources
    Some devices such as NX or the NPU will use some of the XIVE
    provided IPIs for their own interrupts. Thus we need a way for
    those to provide a custom irq_source_ops for portions of the IPI
    space in order for them to provide their own attributes() and
    if needed, interrutps() callbacks.
    
    We achieve that by creating a second list of sources which can
    overlap the primary.
    
    The global stock of IPIs is registered by XIVE in the secondary
    list which is searched when no match is found in the primary.
    
    A new API xive_register_ipi_source() is provided for those devices
    to create an overlapping source structure in the primary list for
    a subset of the IPIs. Those IPIs must have been previously allocated
    using xive_alloc_ipi_irqs()
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

--- hw/phb4.c
+++ hw/phb4.c
@@ -3635,11 +3635,11 @@ static void phb4_create(struct dt_node *np)
 	capp_load_ucode(p);
 
 	/* Register all interrupt sources with XIVE */
-	xive_register_source(p->base_msi, p->num_irqs - 8, 16, p->int_mmio, 0,
-			     NULL, NULL);
-	xive_register_source(p->base_lsi, 8, 16,
-			     p->int_mmio + ((p->num_irqs - 8) << 16),
-			     XIVE_SRC_LSI, p, &phb4_lsi_ops);
+	xive_register_hw_source(p->base_msi, p->num_irqs - 8, 16, p->int_mmio, 0,
+				NULL, NULL);
+	xive_register_hw_source(p->base_lsi, 8, 16,
+				p->int_mmio + ((p->num_irqs - 8) << 16),
+				XIVE_SRC_LSI, p, &phb4_lsi_ops);
 
 	/* Platform additional setup */
 	if (platform.pci_setup_phb)
